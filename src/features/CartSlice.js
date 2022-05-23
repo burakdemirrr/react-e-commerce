0ffe878c91381bdb323bb71eea5688e7fe8b5cd0
Burak <mburak.demir.059@gmail.com>
2022-05-23 20:29:53 +0300

    Redux Integrated

---
+++ src/features/CartSlice.js
@@ -0,0 +1,66 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+
+export const slice=createSlice({
+    name:"tark",
+    initialState:{
+        cartItems:[]
+    },
+    reducers: {
+        addItemToCart: (state, action) => {
+            const pseudoID = (new Date()).getTime();
+
+            state.cartItems.push({
+                id: pseudoID,
+                name:action.payload.product.name,
+                img:action.payload.product.img,
+                productID: action.payload.product.id,
+                quantity: action.payload.quantity,
+                price: action.payload.product.price,
+                totalPrice: action.payload.quantity * action.payload.product.price
+            });
+        },
+        removeFromCart: (state, action) => {
+
+            state.cartItems = state.cartItems.filter(
+                cartItem => cartItem.id !== action.payload.id
+            )
+        },
+        decreaseCart: (state, action) => {
+            const itemIndex = state.cartItems.findIndex(
+                cartItem => cartItem.id === action.payload.id
+            )
+            state.cartItems[itemIndex].quantity += 1;
+            state.cartItems[itemIndex].totalPrice += state.cartItems[itemIndex].price;
+
+        },
+        increaseCart: (state, action) => {
+            const itemIndex = state.cartItems.findIndex(
+                cartItem => cartItem.id === action.payload.id
+            )
+
+            if (state.cartItems[itemIndex].quantity > 1) {
+                state.cartItems[itemIndex].quantity -= 1;
+                state.cartItems[itemIndex].totalPrice -= state.cartItems[itemIndex].price;
+            }
+            else if (state.cartItems[itemIndex].quantity === 1) {
+                state.cartItems = state.cartItems.filter(cartItem => cartItem.id !== state.cartItems[itemIndex].id);
+            }
+        },
+        resetCart:(state)=>{
+            state.cartItems=[];
+        }
+    }
+})
+export const getCartItems = state => state.cart.cartItems;
+
+export const getTotalPrice = state => {
+    return state.cart.cartItems.reduce((total, cartItem) => {
+        return cartItem.totalPrice + total;
+    }, 0);
+}
+
+
+
+export const { addItemToCart, removeFromCart, decreaseCart, increaseCart,resetCart } = slice.actions;
+export default slice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,27 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
-import thunk from 'redux-thunk';
+
 
 import cartReducer from './features/CartSlice';
-
-const reducers = combineReducers({
-  cart: cartReducer,
-});
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['cartReducer'],
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
+import { configureStore } from '@reduxjs/toolkit'
 
 const store = configureStore({
-  reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== 'production',
-  middleware: [thunk],
-});
+  reducer: {
+    tark: cartReducer,
+  },
+})
+
 
 export default store;
